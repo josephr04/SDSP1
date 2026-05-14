4896269b79505e0b33745b76260090c3ef4761cc
J-Jozu <jozu3dx@gmail.com>
2026-05-13 20:29:53 -0500

    Base de datos en xampp

---
+++ Database/conexion.cs
@@ -0,0 +1,15 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Proyecto1_Seguridad.Database
+{
+    public class Conexion
+    {
+        private string connectionString =
+            "server=localhost;database=seguridad_software;user=root;password=;";
+
+        public MySqlConnection ObtenerConexion()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}

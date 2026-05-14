05290f9464920fa9ea855c4f075394f4001eb1c6
J-Jozu <jozu3dx@gmail.com>
2026-05-13 20:31:38 -0500

    Controladores del Login y Autenticacion

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using Proyecto1_Seguridad.Database;
+
+namespace Proyecto1_Seguridad.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            Conexion conexion = new Conexion();
+
+            using (MySqlConnection conn =
+                   conexion.ObtenerConexion())
+            {
+                conn.Open();
+            }
+
+            return View("Login");
+        }
+    }
+}

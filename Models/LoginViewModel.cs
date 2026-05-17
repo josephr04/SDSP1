766cf8faa0acf66db7ce04ef0d90aa40d5ac05f2
josephr04 <jrosas.dev@gmail.com>
2026-05-16 20:09:39 -0500

    Agregar modelos de login y registro

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SDSP1.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "El correo es requerido")]
+        [EmailAddress(ErrorMessage = "Correo no válido")]
+        public string correo { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        [DataType(DataType.Password)]
+        public string contraseña { get; set; }
+    }
+}
---
+++ Models/RegistrarViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SDSP1.Models
+{
+    public class RegistrarViewModel
+    {
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 50 caracteres")]
+        public string nombre { get; set; }
+
+        [Required(ErrorMessage = "El correo es requerido")]
+        [EmailAddress(ErrorMessage = "El correo no es válido")]
+        public string correo { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres")]
+        [DataType(DataType.Password)]
+        public string contraseña { get; set; }
+
+        [Required(ErrorMessage = "Confirma tu contraseña")]
+        [Compare("contraseña", ErrorMessage = "Las contraseñas no coinciden")]
+        [DataType(DataType.Password)]
+        public string confirmarContraseña { get; set; }
+    }
+}

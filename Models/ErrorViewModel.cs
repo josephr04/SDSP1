91acc5af06bb4381da2320948752818691921701
josephr04 <jrosas.dev@gmail.com>
2026-05-17 10:38:43 -0500

    Eliminar archivos generados por defecto

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SDSP1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

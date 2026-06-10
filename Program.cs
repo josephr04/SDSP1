7494b8e34f3ab02560f430f1721bc2b8ca1e93d3
Moises507 <themoisesespinosa507@gmail.com>
2026-06-10 14:26:27 -0500

    Cambios para implementación de bloqueo de IP

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using AspNetCoreRateLimit;
+using Microsoft.AspNetCore.Identity;
+
 using SDSP1.Database;
 using SDSP1.Services;
+using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,12 +25,35 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
 });
 
+// needed to load configuration from appsettings.json
+builder.Services.AddOptions();
+
+// needed to store rate limit counters and ip rules
+builder.Services.AddMemoryCache();
+
+//load general configuration from appsettings.json
+builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+
+//load ip rules from appsettings.json
+builder.Services.Configure<IpRateLimitPolicies>(builder.Configuration.GetSection("IpRateLimitPolicies"));
+
+// inject counter and rules stores
+builder.Services.AddInMemoryRateLimiting();
+//services.AddDistributedRateLimiting<AsyncKeyLockProcessingStrategy>();
+//services.AddDistributedRateLimiting<RedisProcessingStrategy>();
+//services.AddRedisRateLimiting();
+
+// configuration (resolvers, counter key builders)
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+
 var app = builder.Build();
+
+app.UseIpRateLimiting();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Login/Index");   
+    app.UseExceptionHandler("/Login/Index");
     app.UseHsts();
 }
 
@@ -42,3 +69,4 @@ app.MapControllerRoute(
     .WithStaticAssets();
 
 app.Run();
+
